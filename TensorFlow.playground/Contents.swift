adceb9ed379ce1b8cdc280af06668e9dc6ed6a2c
spprichard <spprichard20@gmail.com>
2018-04-27 07:29:17 -0500

    init commit

---
+++ TensorFlow.playground/Contents.swift
@@ -0,0 +1,13 @@
+import TensorFlow
+
+var matrix: Tensor<Float> = [[1,2], [3,4]]
+
+let sum = matrix + matrix
+print("Sum: \(sum)")
+
+let result = sqrt(matrix)
+print("SQRT: \(result)")
+
+let matrixProduct = matrix.dot(matrix)
+print("Matrix Product: \(matrixProduct)")
+
