935639b45316b46551b8be5751becf56704c229e
spprichard <spprichard20@gmail.com>
2018-04-27 12:56:37 -0500

    fixed playground, wrapped tensor ops in func

--- TensorFlow.playground/Contents.swift
+++ TensorFlow.playground/Contents.swift
@@ -1,13 +1,27 @@
 import TensorFlow
 
-var matrix: Tensor<Float> = [[1,2], [3,4]]
+// Wrapping the Tensor op in a fuction is a work around
+// for the bug explained in the README.md
+func tensorSum() {
+    let matrix: Tensor<Float> = [[1,2], [3,4]]
+    let sum = matrix + matrix
+    print("Sum: \(sum)")
+}
+tensorSum()
 
-//let sum = matrix + matrix
-//print("Sum: \(sum)")
 
-let result = sqrt(matrix)
-print("SQRT: \(result)")
+func tensorSqrt(){
+    let matrix: Tensor<Float> = [[1,2], [3,4]]
+    let result = sqrt(matrix)
+    print("SQRT: \(result)")
+}
+tensorSqrt()
 
-let matrixProduct = matrix.dot(matrix)
-print("Matrix Product: \(matrixProduct)")
 
+
+func tensorMatrixProduct() {
+    let matrix: Tensor<Float> = [[1,2], [3,4]]
+    let matrixProduct = matrix.dot(matrix)
+    print("Matrix Product: \(matrixProduct)")
+}
+tensorMatrixProduct()
--- basics.swift
+++ basics.swift
@@ -1,6 +1,14 @@
+#!/usr/bin/env swift -O
+
 import TensorFlow
 
 var matrix: Tensor<Float> = [[1,2], [3,4]]
 
 let sum = matrix + matrix
 print("Sum: \(sum)")
+
+
+let result = sqrt(matrix)
+print("SQRT: \(result)")
+
+
